c29d5a420b14077383926fc5fdf5eab767a21fe9
jk <jk.hao@pku.edu.cn>
2020-06-23 15:23:19 +0800

    完成！

--- compute.c
+++ compute.c
@@ -1,5 +1,6 @@
-//实现词法分析的next函数
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct TOKEN {
     int id;
@@ -7,38 +8,42 @@ typedef struct TOKEN {
 } token;
 
 enum {Num};
+token *tk;
+int read;
 
-token* next(char *src, int *i) {
-    token *tk = malloc(sizeof(token));
+void next(char *src, int *i) {
+    if(read) {
+        read = 0;
+        return;
+    }
     int k = *i;
     while(src[k] == ' ')
         k++;
     if(src[k] > '9' || src[k] < '0') {
         tk->id = src[k];
-        *i = k++;
-        return tk;
+        *i = ++k;
+        return;
     }
     int val = 0;
     while(src[k] <= '9' && src[k] >= '0') {
         val *= 10;
-        val += src[k] - '0';
+        val += src[k++] - '0';
     }
     tk->id = Num;
     tk->value = val;
-    return tk;
+    *i = k;
 }
 
 int factor(char *src, int *i) {
-    token *tk = next(src, i);
+    next(src, i);
     if(tk->id == '(') {
         int val = expr(src, i);
-        tk = next(src, &i);
+        next(src, i);
         if(tk->id != ')')
             printf("error: bracket not match!");
         return val;
     }
     else {
-        tk = next(src,&i);
         if(tk->id != Num)
             printf("error: not a Num!");
         return tk->value;
@@ -46,40 +51,44 @@ int factor(char *src, int *i) {
 }
 
 int term_tail(int lval, char *src, int *i) {
-    token *tk = next(src, i);
+    next(src, i);
+    int val;
     if(tk->id == '*') {
-        tk = next(src, i);
-        return lval * term_tail(tk->value, src, i);
+        val = lval * factor(src, i);
+        return term_tail(val, src, i);
     }
     else if(tk->id == '/') {
-        tk = next(src, i);
-        return lval / term_tail(tk->value, src, i);
+        val = lval / factor(src, i);
+        return term_tail(val, src, i);
     }
-    else if(tk->id != '\n') {
+    else if(tk->id == '\n' || tk->id == '\0') {
         return lval;
     }
     else {
-        printf("error");
-        return -1;
+        read = 1;
+        return lval;
     }
+        
+    
 }
 
 int expr_tail(int lval, char *src, int *i) {
-    token *tk = next(src, i);
+    next(src, i);
+    int val;
     if(tk->id == '+') {
-        tk = next(src, i);
-        return lval * expr_tail(tk->value, src, i);
+        val = lval + term(src, i);
+        return expr_tail(val, src, i);
     }
     else if(tk->id == '-') {
-        tk = next(src, i);
-        return lval / expr_tail(tk->value, src, i);
+        val = lval - term(src, i);
+        return expr_tail(val, src, i);
     }
-    else if(tk->id != '\n') {
+    else if(tk->id == '\n') {
         return lval;
     }
     else {
-        printf("error");
-        return -1;
+        read = 1;
+        return lval;
     }
 }
 
@@ -94,17 +103,29 @@ int expr(char *src, int *i) {
 }
 
 int program(char *src) {
-    int *i = 0;
-    return expr(src, i);
+    int *i = malloc(sizeof(int));
+    *i = 0;
+    int val = expr(src, i);
+    free(i);
+    return val;
 }
 
 
 int main() {
-    int linelen;
-    char *line;
-    while((linelen = getline(&line, 0, stdin) > 0)) {
+    size_t linecap = 0;
+    ssize_t linelen;
+    char *line = NULL;
+    tk = malloc(sizeof(token));
+    read = 0;
+    while((linelen = getline(&line, &linecap, stdin)) > 0) {
+        if(line[0] == '\n')
+            continue;
         int ans = program(line);
+        read = 0;
+        if(line[linelen-1] == '\n')
+            line[linelen-1] = '\0';
+        printf("%s = %d\n", line, ans);
     }
-
+    free(tk);
     return 0;
 }
